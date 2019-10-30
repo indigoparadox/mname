5c77d36c9f9a6d4b4de5cc6e1af5eef43a519ffa
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-10-30 19:47:02 -0400

    Started defining codes and structs.

--- src/mname.h
+++ src/mname.h
@@ -2,5 +2,42 @@
 #ifndef MNAME_H
 #define MNAME_H
 
+#include <stdint.h>
+
+#define M_NAME_RESPONSE_FIELD             0x8000
+
+#define M_NAME_OP_FIELD                   0x7000
+#define M_NAME_OP_STATUS                  0x1000
+
+#define M_NAME_AUTH_ANSWER_FIELD          0x0400
+
+#define M_NAME_TRUNC_FIELD                0x0200
+
+#define M_NAME_RECURSE_REQ_FIELD          0x0100
+
+#define M_NAME_RECURSE_AVAIL_FIELD        0x0080
+
+#define M_NAME_RESPONSE_CODE_FIELD        0x000f
+
+#define M_NAME_RESPONSE_CODE_NONE         0x000
+#define M_NAME_RESPONSE_CODE_FORMAT       0x001
+#define M_NAME_RESPONSE_CODE_SERVER       0x002
+#define M_NAME_RESPONSE_CODE_NAME         0x003
+#define M_NAME_RESPONSE_CODE_REFUSED      0x005
+#define M_NAME_RESPONSE_CODE_YX_DOMAIN    0x006
+#define M_NAME_RESPONSE_CODE_YX_RR_SET    0x007
+#define M_NAME_RESPONSE_CODE_NX_RR_SET    0x008
+#define M_NAME_RESPONSE_CODE_NOT_AUTH     0x009
+#define M_NAME_RESPONSE_CODE_NOT_ZONE     0x010
+
+struct mname_msg {
+   uint16_t id;
+   uint16_t fields;
+   uint16_t questions_len;
+   uint16_t answers_len;
+   uint16_t ns_len;
+   uint16_t addl_len;
+} __attribute__( (packed) );
+
 #endif /* MNAME_H */
 
