4556d1fa8857c6a9f24af7e4491d5e10d6421040
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-10-30 21:36:17 -0400

    Can now listen for DNS query. Can't do much else.

--- src/mquery.c
+++ src/mquery.c
@@ -13,54 +13,60 @@
 
 int main( int argc, char** argv ) {
    int sock = 0;
-   const char* hostname = NULL;
-   const char* portname = "domain";
-   struct addrinfo hints;
-   struct addrinfo* addresses = NULL;
    int res = 0;
+   struct sockaddr_in server;
+   struct sockaddr_in client;
    char buffer[1024] = { 0 };
-   struct sockaddr_storage src_addr;
    ssize_t count = 0;
-   socklen_t src_addr_len = 0;
+   socklen_t client_sz = 0;
+   struct mname_msg* dns_msg = (struct mname_msg*)&buffer;
 
-   memset( &hints, '\0', sizeof( struct addrinfo ) );
-
-   /* Get the local listening address. */
-   hints.ai_family = AF_UNSPEC;
-   hints.ai_socktype = SOCK_DGRAM;
-   hints.ai_protocol = 0;
-   hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
-
-   res = getaddrinfo( hostname, portname, &hints, &addresses );
+   memset( &server, '\0', sizeof( struct sockaddr_in ) );
+   memset( &client, '\0', sizeof( struct sockaddr_in ) );
 
    /* Create the socket. */
-   sock = socket(
-      addresses->ai_family, addresses->ai_socktype, addresses->ai_protocol );
-
+   sock = socket( AF_INET, SOCK_DGRAM, 0 );
    if( 0 > sock ) {
-      fprintf( stderr, "%s\n", strerror( errno ) );
+      fprintf( stderr, "socket(): %s\n", strerror( errno ) );
+      goto cleanup;
+   }
+
+   /* Bind to the DNS port. */
+   server.sin_family = AF_INET;
+   server.sin_port = 53;
+   server.sin_addr.s_addr = INADDR_ANY;
+
+   res = bind( sock, (struct sockaddr*)&server, sizeof( struct sockaddr_in ) );
+   if( 0 > res ) {
+      fprintf( stderr, "bind(): %s\n", strerror( errno ) );
       goto cleanup;
    }
 
    /* Listen for incoming packets. */
-   src_addr_len = sizeof( src_addr ); 
    count = recvfrom( sock, buffer, sizeof( buffer ), 0,
-      (struct sockaddr*)&src_addr, &src_addr_len );
+      (struct sockaddr*)&client, &client_sz );
+   if( 0 > count ) {
+      fprintf( stderr, "recvfrom(): %s\n", strerror( errno ) );
+      goto cleanup;
+   }
 
    /* Handle incoming packets. */
    if( 0 > count ) {
       fprintf( stderr, "%s\n", strerror( errno ) );
       goto cleanup;
    } else if( sizeof( buffer ) == count ) {
-      fprintf( stderr, "Datagram too large; truncated.\n" );
+      fprintf( stderr, "datagram too large; truncated.\n" );
       goto cleanup;
    } else {
    }
 
+   printf( "dns:\nis_response(): %d\n nslen: %d\n",
+      m_name_is_response( dns_msg ), dns_msg->ns_len );
+
 cleanup:
 
-   if( NULL != addresses ) {
-      freeaddrinfo( addresses );
+   if( 0 < sock ) {
+      close( sock );
    }
 
    return res;
