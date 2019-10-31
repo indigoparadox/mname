7f9fa1cef5fa5974cfbc4587c9f1fc88e1c6d311
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-10-30 22:51:56 -0400

    Listen indefinitely and give some info.

--- src/mquery.c
+++ src/mquery.c
@@ -11,6 +11,8 @@
 #include <string.h>
 #include <errno.h>
 
+#define NAME_BUF_LEN 512
+
 int main( int argc, char** argv ) {
    int sock = 0;
    int res = 0;
@@ -20,6 +22,8 @@ int main( int argc, char** argv ) {
    ssize_t count = 0;
    socklen_t client_sz = sizeof( struct sockaddr_storage );
    struct mname_msg* dns_msg = (struct mname_msg*)&buffer;
+   int running = 1;
+   char domain_name[NAME_BUF_LEN] = { 0 };
 
    memset( &server, '\0', sizeof( struct sockaddr_in ) );
    memset( &client, '\0', sizeof( struct sockaddr_in ) );
@@ -33,7 +37,7 @@ int main( int argc, char** argv ) {
 
    /* Bind to the DNS port. */
    server.sin_family = AF_INET;
-   server.sin_port = 53;
+   server.sin_port = 53; /* Yes, this is wrong, but it's unpriv. */
    server.sin_addr.s_addr = htonl( INADDR_ANY );
 
    res = bind( sock, (struct sockaddr*)&server, sizeof( struct sockaddr_in ) );
@@ -42,35 +46,44 @@ int main( int argc, char** argv ) {
       goto cleanup;
    }
 
-   /* Listen for incoming packets. */
-   count = recvfrom( sock, buffer, sizeof( buffer ), 0,
-      (struct sockaddr*)&client, &client_sz );
-   if( 0 > count ) {
-      fprintf( stderr, "recvfrom(): %s\n", strerror( errno ) );
-      goto cleanup;
-   }
+   while( running ) {
 
-   /* Handle incoming packets. */
-   if( 0 > count ) {
-      fprintf( stderr, "%s\n", strerror( errno ) );
-      goto cleanup;
-   } else if( sizeof( buffer ) == count ) {
-      fprintf( stderr, "datagram too large; truncated to: %ld\n", count );
-      goto cleanup;
-   } else {
-   }
+      /* Listen for incoming packets. */
+      count = recvfrom( sock, buffer, sizeof( buffer ), 0,
+         (struct sockaddr*)&client, &client_sz );
+      if( 0 > count ) {
+         fprintf( stderr, "recvfrom(): %s\n", strerror( errno ) );
+         goto cleanup;
+      }
 
-   printf( "dns:\n cli_addr_sz: %d\n is_response(): %d\n nslen: %d\n",
-      client_sz, m_name_is_response( dns_msg ), dns_msg->ns_len );
+      /* Handle incoming packets. */
+      if( 0 > count ) {
+         fprintf( stderr, "%s\n", strerror( errno ) );
+         goto cleanup;
+      } else if( sizeof( buffer ) == count ) {
+         fprintf( stderr, "datagram too large; truncated to: %ld\n", count );
+         goto cleanup;
+      } else {
+      }
 
-   mname_response( dns_msg );
+      memset( domain_name, '\0', NAME_BUF_LEN );
+      mname_get_q_domain( dns_msg, domain_name, NAME_BUF_LEN );
 
-   /* Send response back to requester. */
-   count = sendto( sock, buffer, count, 0,
-      (struct sockaddr*)&client, client_sz );
-   if( 0 > count ) {
-      fprintf( stderr, "sendto(): %s\n", strerror( errno ) );
-      goto cleanup;
+      printf( "dns:\n cli_addr_sz: %d\n is_response(): %d\n nslen: %d\n"
+         " questions: %d\n domain: %s\n",
+         client_sz, m_name_is_response( dns_msg ), dns_msg->ns_len,
+         m_htons( dns_msg->questions_len ), domain_name );
+
+      mname_response( dns_msg );
+
+      /* Send response back to requester. */
+      count = sendto( sock, buffer, count, 0,
+         (struct sockaddr*)&client, client_sz );
+      if( 0 > count ) {
+         fprintf( stderr, "sendto(): %s\n", strerror( errno ) );
+         goto cleanup;
+      }
+
    }
 
 cleanup:
